fb660c48f92d8864f0fb6cbf1d1f267402c0b907
Yoodahun <dahun4032@gmail.com>
2021-02-12 22:57:04 +0900

    ADD User model

---
+++ models/User.py
@@ -0,0 +1,11 @@
+
+class User:
+    def __init__(self, user_id, username, password):
+        self.user_id = user_id
+        self.user_name = username
+        self.user_password = password
+        self.access_token = ""
+        self.refresh_token = ""
+
+
+
