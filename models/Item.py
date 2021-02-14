a6743e7d3fcacb788640e9a6becb4d8150cc0a30
Yoodahun <dahun4032@gmail.com>
2021-02-14 16:21:20 +0900

    ADD item model

---
+++ models/Item.py
@@ -0,0 +1,8 @@
+
+class Item():
+
+    def __init__(self, _id, name, price, store_id):
+        self.item_id = _id
+        self.item_name = name
+        self.item_price = price
+        self.item_store_id = store_id
