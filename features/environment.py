53502ef1bbc8531e8d8c12cc1e332ac4cd368673
Yoodahun <dahun4032@gmail.com>
2021-02-14 18:42:40 +0900

    ADD item attribute

--- features/environment.py
+++ features/environment.py
@@ -3,22 +3,30 @@ from models.User import User
 
 user = None
 store = None
+item = None
 
 
 def before_scenario(context, scenario):
     global user
     global store
+    global item
 
     if user is not None:
         context.user = user
     if store is not None:
         context.store = store
+    if item is not None:
+        context.item = item
 
 
 def after_scenario(context, scenario):
     global user
     global store
+    global item
+
     if context.user:
         user = context.user
     if context.store:
         store = context.store
+    if context.item:
+        item = context.item
