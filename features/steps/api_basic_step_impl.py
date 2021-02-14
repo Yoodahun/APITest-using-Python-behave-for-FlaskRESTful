d5a30ab50a8a9344d41549137b30a37c00f43272
Yoodahun <dahun4032@gmail.com>
2021-02-14 15:18:43 +0900

    ADD logging and check user has been logged in

--- features/steps/api_basic_step_impl.py
+++ features/steps/api_basic_step_impl.py
@@ -1,5 +1,6 @@
 from behave import *
 import requests
+import logging
 import contants.status_code_constants
 
 API_URI = "https://practice-flask-restful-dh.herokuapp.com"
@@ -7,19 +8,19 @@ header = {
     "content-type": "application/json"
 }
 
+log = logging.getLogger("api_basic_step_impl")
 
 @then(u'response status code is {status_code:d}')
 def check_status_code(context, status_code):
-    print(context.response.status_code)
+    log.info(context.response)
     assert context.response.status_code == status_code
 
 
 @step('"{key_name}" value in response body is equal to "{input_value}"')
 def check_equal_response_and_request_value(context, key_name, input_value):
-
     response_body = context.response.json()
     print(response_body)
-
+    log.info("check_equal_response_and_request_value " + response_body[key_name])
     assert response_body[key_name] == input_value
 
 
@@ -30,3 +31,8 @@ def check_key_value(context, key1, key2):
 
     assert key1 in response_body.keys()
     assert key2 in response_body.keys()
+
+
+@step("user has been logged in")
+def step_impl(context):
+    assert context.user is not None
