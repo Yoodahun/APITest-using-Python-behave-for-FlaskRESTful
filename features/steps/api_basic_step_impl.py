12c8114de844c033ca85305e22f11dca77f32860
Yoodahun <dahun4032@gmail.com>
2021-02-14 18:41:17 +0900

    UPDATE header value, assert type converting ADD logging code

--- features/steps/api_basic_step_impl.py
+++ features/steps/api_basic_step_impl.py
@@ -5,29 +5,29 @@ import contants.status_code_constants
 
 API_URI = "https://practice-flask-restful-dh.herokuapp.com"
 header = {
-    "content-type": "application/json"
+    "Content-Type": "application/json"
 }
 
 log = logging.getLogger("api_basic_step_impl")
 
 @then(u'response status code is {status_code:d}')
 def check_status_code(context, status_code):
-    log.info(context.response)
+    log.info(f"check status code {context.response}")
+    log.info(f"check status code {context.response.json()}")
     assert context.response.status_code == status_code
 
 
 @step('"{key_name}" value in response body is equal to "{input_value}"')
 def check_equal_response_and_request_value(context, key_name, input_value):
     response_body = context.response.json()
-    print(response_body)
-    log.info("check_equal_response_and_request_value " + response_body[key_name])
-    assert response_body[key_name] == input_value
+    log.info("check_equal_response_and_request_value " + str(response_body[key_name]))
+    assert str(response_body[key_name]) == str(input_value)
 
 
 @step('response body has key that "{key1}" and "{key2}"')
 def check_key_value(context, key1, key2):
     response_body = context.response.json()
-    print(response_body)
+    log.info(f" check_key_value {response_body}")
 
     assert key1 in response_body.keys()
     assert key2 in response_body.keys()
