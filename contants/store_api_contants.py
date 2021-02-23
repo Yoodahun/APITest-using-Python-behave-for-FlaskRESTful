cd1eab61e2ce3b9c311848d902899cbfbb3bc7c2
Yoodahun <dahun4032@gmail.com>
2021-02-23 15:03:02 +0900

    UPDATE return message

--- contants/store_api_contants.py
+++ contants/store_api_contants.py
@@ -3,5 +3,9 @@ STORE_DELETED = "Store deleted"
 ISSUE_CREATING_THE_STORE_DB = "An error occurred while creating the store"
 
 
-def store_name_alreadt_exists(store_name):
-    return f"A Store with name {store_name} already exists."
+def store_name_already_exists(store_name):
+    return f"A Store with name '{store_name}' already exists."
+
+
+def store_is_not_exists(store_name):
+    return f"Store '{store_name}' is not exists."
